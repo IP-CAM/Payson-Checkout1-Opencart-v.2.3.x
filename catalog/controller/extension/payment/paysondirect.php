ab75a223718a2ad84c1ac13f6493b375bc6a57fa
oscar-vil <oscar.villegas@payson.se>
2020-11-02 10:40:01 +0100

    2.0.0.5
    Updated PHP-lib

--- catalog/controller/extension/payment/paysondirect.php
+++ catalog/controller/extension/payment/paysondirect.php
@@ -7,7 +7,7 @@ class ControllerExtensionPaymentPaysondirect extends Controller {
     private $isInvoice;
     private $data = array();
 
-    const MODULE_VERSION = 'Aion_2.0.0.4';
+    const MODULE_VERSION = 'Aion_2.0.0.5';
 
     function __construct($registry) {
         parent::__construct($registry);
